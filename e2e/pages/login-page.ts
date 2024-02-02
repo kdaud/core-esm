96da0d44b8edbc8d3d205225d0359eb7b3d46548
kdaud <dkakumirizii@gmail.com>
2024-02-02 11:18:54 +0300

    Add seperate test for logout feature

--- e2e/pages/login-page.ts
+++ e2e/pages/login-page.ts
@@ -1,9 +1,22 @@
-import { Page } from '@playwright/test';
+import { type Page } from '@playwright/test';
 
 export class LoginPage {
   constructor(readonly page: Page) {}
 
   async goto() {
     await this.page.goto(`${process.env.E2E_BASE_URL}/spa/login`);
+  }
+
+  async enterLoginCredentials() {
+    await this.page.getByLabel(/username/i).fill(`${process.env.E2E_USER_ADMIN_USERNAME}`);
+    await this.page.getByText(/continue/i).click();
+    await this.page.getByLabel(/password/i).fill(`${process.env.E2E_USER_ADMIN_PASSWORD}`);
+    await this.page.getByRole('button', { name: /log in/i }).click();
+    await this.page.getByText(/inpatient ward/i).click();
+    await this.page.getByRole('button', { name: /confirm/i }).click();
+  }
+
+  async gotoHomePage() {
+    await this.page.goto('home');
   }
 }
